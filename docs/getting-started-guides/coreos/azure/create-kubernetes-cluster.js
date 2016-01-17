79effcc455d44b30e1c775617f6732137eaccec3
Phong Nguyen <pnguyen@hydrasoft.io>
2016-01-17 09:09:37 -0500

    Add variables for kube instances

--- docs/getting-started-guides/coreos/azure/create-kubernetes-cluster.js
+++ docs/getting-started-guides/coreos/azure/create-kubernetes-cluster.js
@@ -3,8 +3,10 @@
 var azure = require('./lib/azure_wrapper.js');
 var kube = require('./lib/deployment_logic/kubernetes.js');
 var service_name = process.env['AZ_SERVICE_NAME'] || 'kube';
+var etcd_instances = process.env['AZ_ETCD_INSTANCES'] || 3;
+var kube_instances = process.env['AZ_KUBE_INSTANCES'] || 3;
 
-azure.create_config(service_name, { 'etcd': 3, 'kube': 3 });
+azure.create_config(service_name, { 'etcd': etcd_instances, 'kube': kube_instances });
 
 azure.run_task_queue([
   azure.queue_default_network(),
